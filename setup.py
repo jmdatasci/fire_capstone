81dd601c3e7dbbee46f4d59bb6b47ce575208736
Jordan <16210031+jmdatasci@users.noreply.github.com>
2023-01-31 09:58:28 -0500

    Setting up project structure
    Setting up project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science Capstone Project to Analyze Wildfire impact on Pediatric Health in California',
+    author='DKMMN',
+    license='MIT',
+)
